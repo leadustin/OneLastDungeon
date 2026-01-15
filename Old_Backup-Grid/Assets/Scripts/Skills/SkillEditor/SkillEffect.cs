16c9e92f40d6580861cb5373e8f14117c11e95ad
leadustin <155164500+leadustin@users.noreply.github.com>
2026-01-15 20:20:05 +0100

    Delete Old_Backup-Grid/Assets directory

--- Old_Backup-Grid/Assets/Scripts/Skills/SkillEditor/SkillEffect.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-// Das ist die Basisklasse für alle Effekte.
-public abstract class SkillEffect : ScriptableObject
-{
-    // Wir übergeben jetzt 'stepData', damit der Effekt weiß, wie stark er sein soll.
-    public abstract void Activate(GameObject caster, SkillStep stepData);
-}
